7189d456de0a57c87e7429b25c934d9bb2b4f2a3
admintrial <admintrial@ad.samsarageorgia.com>
2024-09-09 12:39:18 +0400

    Your commit message here

--- src/app/my-github-account/my-github-account.component.ts
+++ src/app/my-github-account/my-github-account.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-my-github-account',
   standalone: true,
   imports: [],
   templateUrl: './my-github-account.component.html',
-  styleUrl: './my-github-account.component.scss'
+  styleUrls: ['./my-github-account.component.scss']
 })
-export class MyGithubAccountComponent {
+export class MyGithubAccountComponent implements AfterViewInit {
 
+  @ViewChild('backgroundVideo') backgroundVideo!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit(): void {
+    const video = this.backgroundVideo.nativeElement;
+    
+    // Ensures autoplay when the video is ready
+    video.addEventListener('canplay', () => {
+      video.play();
+    });
+
+    // Trigger video play programmatically
+    video.muted = true; // Ensure it's muted for autoplay to work
+    video.play().catch(error => {
+      console.error('Error in video playback:', error);
+    });
+  }
 }
